cf956e44a046f3230dfe88dfa20d3ab36ae895d6
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-05-15 03:20:21 +0100

    bisectionmethod

---
+++ bisectionmethod.py
@@ -0,0 +1,29 @@
+print('Hello world')
+##DATA TYPES
+# Integer
+# list
+# strings
+# bool
+# tuple
+print(f"hi")  #formatted qoute
+
+# # anythinfg in between normal () is called tuple
+# anything in between [] is called a list
+# lenghth of a list is the no of elements in the list
+
+a=2
+print(a)
+# two types of loop: for and while loop
+for a in [1,2,3]:
+    print(a)
+
+# conditions
+if(2==2):
+    print('yes')
+
+print(2==2)
+
+# Assignment
+# Read on functions, learn the synthax for ur for and while loops
+
+
